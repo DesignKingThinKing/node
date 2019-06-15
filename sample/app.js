90c18be47e5d3ae7e904a0e1d68d3f2c36250a46
jihyyeshin <tlsjh082@gmail.com>
2019-06-15 18:44:13 +0900

    190615 connect to mysql server

--- sample/app.js
+++ sample/app.js
@@ -1,17 +1,50 @@
 var express = require('express')
 var app = express()
+var bodyParser = require('body-parser')
 
-app.get('/', function (req, res) {
-    res.send("Hello, World!")
+app.use(bodyParser.urlencoded({ extended: true }))
+
+//mysql 모듈 불러오기
+var mysql = require('mysql')
+
+//mysql 커넥션 생성
+var connection = mysql.createConnection({
+    host : "127.0.0.1", //서버 로컬 IP
+    port : 3306,
+    user : "root", //계정 아이디
+    password : "1234", //계정 비밀번호
+    database : "test" //접속할 DB
 })
 
-app.get('/pass', function (req, res) {
 
-    var data = req.query.data
-    res.send(data)
+//mysql 접속
+connection.connect()
+
+// user 라우터
+app.post('/user', function (req, res) {
+
+    var userID = req.body.id
+    var userPW = req.body.pw
+
+    if(userID && userPW) { // userID와 userPW가 유효하다면
+
+        //SQL문 실행
+        connection.query("INSERT INTO user (userID, userPW) VALUES ('"+ userID +"', '"+userPW+"')" , 
+            function (error, result, fields) {
+
+            if (error) { //에러 발생시
+                res.send('err : ' + error)
+            }
+            else { //실행 성공
+                console.log( userID + ',' + userPW )
+                res.send('success create user name: '+ userID +' pw: ' + userPW)
+            }
+        })
+    }
 
 })
 
+//서버 시작
 app.listen(8080, function () {
     console.log("server starting with 8080")
 })
